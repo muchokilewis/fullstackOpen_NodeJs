6f9f1872a7876a9fabf6b19f21e0dd4cd91cda4f
uptownkiddo <101928225+uptownkiddo@users.noreply.github.com>
2024-05-14 15:07:20 +0300

    step 7 - added morgan middleware

--- part3/Phonebook backend/index.js
+++ part3/Phonebook backend/index.js
@@ -1,7 +1,9 @@
-// const http = require('http')
-
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
+
+app.use(morgan('tiny'))
 
 app.use(express.json())
 
