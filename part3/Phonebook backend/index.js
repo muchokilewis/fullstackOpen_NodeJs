7cfe005b456733bc83ff0e28a1ae61504ffd752b
uptownkiddo <101928225+uptownkiddo@users.noreply.github.com>
2024-05-14 10:38:42 +0300

    Phonebook backend step 2

--- part3/Phonebook backend/index.js
+++ part3/Phonebook backend/index.js
@@ -1,4 +1,9 @@
-const http = require('http')
+// const http = require('http')
+
+const express = require('express')
+const app = express()
+
+app.use(express.json())
 
 let phonebook = [
     { 
@@ -22,24 +27,40 @@ let phonebook = [
         "number": "39-23-6423122"
     }
 ]
+console.log(phonebook.length)
 
-const server = http.createServer((request, response) => {
-    if (request.method === 'GET' && request.url === '/api/persons'){
-        response.statusCode = 200
-        response.setHeader('Content-Type', 'application/json')
-        response.end(JSON.stringify(phonebook))
-    }
-    else if (request.method === 'GET' && request.url === '/'){
-        response.end('<h1>Hello There</h1>\n<p>For the phonebook, the path is "/api/persons". ')
-    }
-    else {
-        response.statusCode = 404
-        response.end()
-    }
+// const server = http.createServer((request, response) => {
+//     if (request.method === 'GET' && request.url === '/api/persons'){
+//         response.statusCode = 200
+//         response.setHeader('Content-Type', 'application/json')
+//         response.end(JSON.stringify(phonebook))
+//     }
+//     else if (request.method === 'GET' && request.url === '/'){
+//         response.end('<h1>Hello There</h1>\n<p>For the phonebook, the path is "/api/persons". ')
+//     }
+//     else {
+//         response.statusCode = 404
+//         response.end()
+//     }
+// })
+
+
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello there</h1><h3>Paths:</h3><p>info : "/info"</p><p>phonebook: "/api/persons" </p>')
+})
+
+app.get('/api/persons',(request, response) => {
+    response.json(phonebook)
+})
+
+app.get('/info', (request, response) => {
+    const now = new Date()
+    response.send(`<p>Phonebook has info for ${phonebook.length} people</p><br><p>${now}`)
 })
 
 const PORT = 3001
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
 
