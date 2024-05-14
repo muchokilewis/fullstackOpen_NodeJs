941f159f3c80c6d65213ccdeddf32bd7778b044f
uptownkiddo <101928225+uptownkiddo@users.noreply.github.com>
2024-05-14 10:57:49 +0300

    Phonebook backend step 3

--- part3/Phonebook backend/index.js
+++ part3/Phonebook backend/index.js
@@ -5,7 +5,7 @@ const app = express()
 
 app.use(express.json())
 
-let phonebook = [
+let persons = [
     { 
         "id": 1,
         "name": "Arto Hellas", 
@@ -27,41 +27,33 @@ let phonebook = [
         "number": "39-23-6423122"
     }
 ]
-console.log(phonebook.length)
-
-// const server = http.createServer((request, response) => {
-//     if (request.method === 'GET' && request.url === '/api/persons'){
-//         response.statusCode = 200
-//         response.setHeader('Content-Type', 'application/json')
-//         response.end(JSON.stringify(phonebook))
-//     }
-//     else if (request.method === 'GET' && request.url === '/'){
-//         response.end('<h1>Hello There</h1>\n<p>For the phonebook, the path is "/api/persons". ')
-//     }
-//     else {
-//         response.statusCode = 404
-//         response.end()
-//     }
-// })
-
-
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello there</h1><h3>Paths:</h3><p>info : "/info"</p><p>phonebook: "/api/persons" </p>')
 })
 
 app.get('/api/persons',(request, response) => {
-    response.json(phonebook)
+    response.json(persons)
 })
 
 app.get('/info', (request, response) => {
     const now = new Date()
-    response.send(`<p>Phonebook has info for ${phonebook.length} people</p><br><p>${now}`)
+    response.send(`<p>Phonebook has info for ${persons.length} people</p><br><p>${now}`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    }
+    else {
+        response.status(404).end()
+    }
 })
 
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
-
-//Implement a Node application that returns a hardcoded list of phonebook entries from the address http://localhost:3001/api/persons.
